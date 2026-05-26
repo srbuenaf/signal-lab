1096a926a99356aefbb77d89bbaf56ba4d533362
srbuenaf <srbuenaf@ull.edu.es>
2026-05-26 16:28:52 +0100

    Estado global separado en state.js

---
+++ js/state.js
@@ -0,0 +1,16 @@
+const state = {
+
+    isConvolution: true,
+    isCausal: false,
+    isAutoScale: true,
+    isPeriodicMode: false,
+
+    signalType: 'ruido',
+    filterType: 'exp',
+
+    freq: 1,
+    width: 40,
+
+    sliderT: 250,
+    t: 0
+};
